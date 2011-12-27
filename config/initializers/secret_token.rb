aba6b891ae5aa0ee958cc55653e267ee9cf318ab
Robinson Duffy <robinson.duffy@gmail.com>
2011-12-26 19:02:04 -0900

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Akmap::Application.config.secret_token = '6d138308d2cf485d87b906276013e4e4d6553fccd51a83fb2a45c08fcbb872107b7f7ed9573986e50c315f62b7518c33a8bab55eb9f13986b7bdb9ebeff7ba5d'
